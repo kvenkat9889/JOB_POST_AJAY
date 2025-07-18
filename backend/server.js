775486b6086c0b15726ca3882006e73523c1437f
Nikhilesh <nikhileshgowda10@gmail.com>
2025-07-18 11:23:42 -0700

    commit4

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const path = require('path');
 const cors = require('cors'); // Add CORS support
 const app = express();
-const port = process.env.PORT || 3411;
+const port = process.env.PORT || 3059;
 
 // PostgreSQL connection
 const pool = new Pool({
@@ -19,9 +19,9 @@ const pool = new Pool({
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
-    "http://44.202.56.233:8020",
+    "http://44.202.56.233:8188",
     "http://127.0.0.1:5500",
-    "http://44.202.56.233:8021",
+    "http://44.202.56.233:8189",
    
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
